4546e4824e08a8ff2251e00ec0498c0e9b1a8c59
tanluc2601 <tantanluc08@gmail.com>
2022-11-06 08:08:55 +0700

    add_file1

---
+++ stm32/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Nov 5, 2022
+ *      Author: tanta
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
